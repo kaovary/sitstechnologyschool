ca02b5119ce42339a79ec79d19ff0e43cb463799
kaovary <kaovary12@gmail.com>
2025-09-13 13:29:09 +0700

    update

--- components/Breadcrumb.tsx
+++ components/Breadcrumb.tsx
@@ -6,58 +6,58 @@ import Link from 'next/link';
 import SlideHome from './home/SlideHome';
 
 export default function Breadcrumb() {
-    const breadcrumb = useBreadcrumbStore((state) => state.breadcrumb);
-    const setBreadcrumb = useBreadcrumbStore((state) => state.setBreadcrumb);
-    const pathname = usePathname();
+    // const breadcrumb = useBreadcrumbStore((state) => state.breadcrumb);
+    // const setBreadcrumb = useBreadcrumbStore((state) => state.setBreadcrumb);
+    // const pathname = usePathname();
 
-    useEffect(() => {
-        if (!pathname) return; // Handle null pathname gracefully
+    // useEffect(() => {
+    //     if (!pathname) return; // Handle null pathname gracefully
 
-        if (pathname === "/") {
-            setBreadcrumb({ title: "Home", url: "/", image: "/assets/img/course/course1.jpg" });
-        }
-        else if (pathname.toLowerCase().includes("/about")) {
-            setBreadcrumb({ title: "About", url: "/about", image: "/assets/img/course/course2.jpg" });
-        }
-        else if (pathname.toLowerCase().includes("/courses")) {
-            setBreadcrumb({ title: "Courses", url: "/courses", image: "/assets/img/course/course3.jpg" });
-        }
-        else if (pathname.toLowerCase().includes("/facility")) {
-            setBreadcrumb({ title: "Facility", url: "/facility", image: "/assets/img/course/course4.jpg" });
-        }
-        else if (pathname.toLowerCase().includes("/history_logo")) {
-            setBreadcrumb({ title: "History & Logo Meaning", url: "/history_logo", image: "/assets/img/course/course5.jpg" });
-        }
-        else if (pathname.toLowerCase().includes("/school_structure")) {
-            setBreadcrumb({ title: "School Structure", url: "/school_structure", image: "/assets/img/course/course6.jpg" });
-        }
-        else if (pathname.toLowerCase().includes("/vision_mission_corevalue")) {
-            setBreadcrumb({ title: "Vision Mission & Core Values", url: "/vision_mission_corevalue", image: "/assets/img/course/course7.jpg" });
-        }
-        else if (pathname.toLowerCase().includes("/location")) {
-            setBreadcrumb({ title: "Location", url: "/location", image: "/assets/img/course/course8.jpg" });
-        }
-        else if (pathname.toLowerCase().includes("/partner")) {
-            setBreadcrumb({ title: "Partner", url: "/partner", image: "/assets/img/course/course9.jpg" });
-        }
-        else if (pathname.toLowerCase().includes("/news")) {
-            setBreadcrumb({ title: "News", url: "/news", image: "/assets/img/course/course10.jpg" });
-        }
-        else if (pathname.toLowerCase().includes("/register")) {
-            setBreadcrumb({ title: "Register", url: "/register", image: "/assets/img/course/course1.jpg" });
-        }
-        else if (pathname.toLowerCase().includes("/testimonial")) {
-            setBreadcrumb({ title: "Testimonial", url: "/testimonial", image: "/assets/img/course/course2.jpg" });
-        }
-        else if (pathname.toLowerCase().includes("/contact")) {
-            setBreadcrumb({
-                title: "Contact", url: "/contact", image: "/assets/img/carousel-2.jpg"
-            });
-        } else {
-            setBreadcrumb({ title: "Home", url: "/", image: "/assets/img/carousel-1.jpg" });
-        }
+    //     if (pathname === "/") {
+    //         setBreadcrumb({ title: "Home", url: "/", image: "/assets/img/course/course1.jpg" });
+    //     }
+    //     else if (pathname.toLowerCase().includes("/about")) {
+    //         setBreadcrumb({ title: "About", url: "/about", image: "/assets/img/course/course2.jpg" });
+    //     }
+    //     else if (pathname.toLowerCase().includes("/courses")) {
+    //         setBreadcrumb({ title: "Courses", url: "/courses", image: "/assets/img/course/course3.jpg" });
+    //     }
+    //     else if (pathname.toLowerCase().includes("/facility")) {
+    //         setBreadcrumb({ title: "Facility", url: "/facility", image: "/assets/img/course/course4.jpg" });
+    //     }
+    //     else if (pathname.toLowerCase().includes("/history_logo")) {
+    //         setBreadcrumb({ title: "History & Logo Meaning", url: "/history_logo", image: "/assets/img/course/course5.jpg" });
+    //     }
+    //     else if (pathname.toLowerCase().includes("/school_structure")) {
+    //         setBreadcrumb({ title: "School Structure", url: "/school_structure", image: "/assets/img/course/course6.jpg" });
+    //     }
+    //     else if (pathname.toLowerCase().includes("/vision_mission_corevalue")) {
+    //         setBreadcrumb({ title: "Vision Mission & Core Values", url: "/vision_mission_corevalue", image: "/assets/img/course/course7.jpg" });
+    //     }
+    //     else if (pathname.toLowerCase().includes("/location")) {
+    //         setBreadcrumb({ title: "Location", url: "/location", image: "/assets/img/course/course8.jpg" });
+    //     }
+    //     else if (pathname.toLowerCase().includes("/partner")) {
+    //         setBreadcrumb({ title: "Partner", url: "/partner", image: "/assets/img/course/course9.jpg" });
+    //     }
+    //     else if (pathname.toLowerCase().includes("/news")) {
+    //         setBreadcrumb({ title: "News", url: "/news", image: "/assets/img/course/course10.jpg" });
+    //     }
+    //     else if (pathname.toLowerCase().includes("/register")) {
+    //         setBreadcrumb({ title: "Register", url: "/register", image: "/assets/img/course/course1.jpg" });
+    //     }
+    //     else if (pathname.toLowerCase().includes("/testimonial")) {
+    //         setBreadcrumb({ title: "Testimonial", url: "/testimonial", image: "/assets/img/course/course2.jpg" });
+    //     }
+    //     else if (pathname.toLowerCase().includes("/contact")) {
+    //         setBreadcrumb({
+    //             title: "Contact", url: "/contact", image: "/assets/img/carousel-2.jpg"
+    //         });
+    //     } else {
+    //         setBreadcrumb({ title: "Home", url: "/", image: "/assets/img/carousel-1.jpg" });
+    //     }
 
-    }, [pathname, setBreadcrumb]);
+    // }, [pathname, setBreadcrumb]);
 
     return (
         <>
